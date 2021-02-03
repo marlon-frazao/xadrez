c0ae4a131b0ae78369e6d807f7742fd122401342
marlon-frazao <frazao.marlon@gmail.com>
2021-02-03 09:47:10 -0300

    Correcao - melhor tratamento para promoca

--- src/chess/ChessMatch.java
+++ src/chess/ChessMatch.java
@@ -1,6 +1,5 @@
 package chess;
 
-import java.security.InvalidParameterException;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.stream.Collectors;
@@ -136,7 +135,7 @@ public class ChessMatch {
 			throw new IllegalStateException("Nao ha peca para promover!");
 		}
 		if (!type.equals("B") && !type.equals("N") && !type.equals("R") && !type.equals("Q")) {
-			throw new InvalidParameterException("Tipo invalido para promocao!");
+			return promoted;
 		}
 
 		Position pos = promoted.getChessPosition().toPosition();
